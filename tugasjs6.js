6eb19942c6d4f9b37517a1533092d2508b5fdc65
Raihan Arya <raihanarya456@gmail.com>
2022-05-28 19:10:00 +0700

    Tugas  JS
    Tugas  JS

---
+++ tugasjs6.js
@@ -0,0 +1,11 @@
+function tugas6() {
+    for (var i = 1; i <= 20; i++) {
+        if (i % 2 == 0 && i != 0) {
+            console.log("genap");
+        } else {
+            console.log(i);
+        }
+    }
+}
+
+tugas6(); 
